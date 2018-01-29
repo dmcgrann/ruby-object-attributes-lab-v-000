66db70b0a172b604f02cd9ad4205650630898862
David McGrann <dpmcgrann@gmail.com>
2018-01-29 22:35:43 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name)
+    @name = breed
+  end
+
+  def breed
+    @name
+  end
+
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+    
